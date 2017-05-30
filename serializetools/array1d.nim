5e7072c35ce7544111ea1fe0f03ed85f87be2a76
Robert Edwards <edwards@jlab.org>
2017-05-29 22:07:48 -0400

    Added a hash so the Array1dO can be used in a Table. Added some tests in a isMainModule.

--- serializetools/array1d.nim
+++ serializetools/array1d.nim
@@ -1,5 +1,7 @@
 ## Support for 1D arrays (sequences) that are 1-based
 
+import hashes
+
 type
   Array1dO*[T] = object
     data*: seq[T]
@@ -18,4 +20,32 @@ proc `[]`*[T](a: Array1dO[T], i:Positive): T =
 
 proc `[]=`*[T](a: var Array1dO[T], i:Positive): T =
     result = a.data[i-1]
+
+proc hash*[T](x: Array1dO[T]): Hash =
+  result = x.data.hash
+
+#----------------------------------------------------------------------------
+when isMainModule:
+  import tables
+
+  # Define the array first
+  var fred: Array1dO[int]
+  fred.data = @[1, 7, -5]
+  echo "fred = ", fred
+
+  var sally: Array1dO[int]
+  sally.data = @[3, 4, 5]
+  echo "sally = ", sally
+
+  var george: Array1dO[int]
+  george.data = @[-3, -2, -1]
+  echo "george = ", george
+
+  # Now make a table out of it
+  var foo = initTable[type(fred), string]()
+  foo.add(fred,   "boo")
+  foo.add(sally,  "foo")
+  foo.add(george, "roo")
+
+  echo "Table= ", foo
 
